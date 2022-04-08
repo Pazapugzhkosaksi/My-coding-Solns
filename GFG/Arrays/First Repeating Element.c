bec91f42f63f225227260247e00152fb7d5931d8
Pazapugzhkosaksi <Pazapugazhkosaksi@gmail.com>
2022-04-08 15:21:17 +0530

    Updated on 08-04-2022

--- GFG/Arrays/First Repeating Element.c
+++ GFG/Arrays/First Repeating Element.c
@@ -13,3 +13,27 @@ int firstRepeated(int arr[], int n) {
         }
         return -1;
     }
+
+
+// Successful Approach
+int firstRepeated(int arr[], int n) {
+        unordered_map<int,int> umap;
+        int min=65535;
+        for(int i=0;i<n;i++)
+        {
+            if(umap.find(arr[i])==umap.end())
+                umap[arr[i]]=i;
+            else
+            {
+                unordered_map<int,int>::const_iterator iter=umap.find(arr[i]);
+                //cout<<iter->first<<":"<<iter->second<<endl;
+                if(iter->second<min)
+                    min=iter->second;
+                //return iter->second;
+            }
+        }
+        if(min!=65535)
+            return min+1;
+        else
+            return -1;
+    }
